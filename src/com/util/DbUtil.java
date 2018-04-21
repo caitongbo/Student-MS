e418f65ea116c7c8630d7362caa73c83a4b1b898
caitongbo <461892420@qq.com>
2018-04-21 23:38:13 +0800

    first commit

---
+++ src/com/util/DbUtil.java
@@ -0,0 +1,33 @@
+package com.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import com.util.DbUtil;
+import com.util.PropertiesUtil;
+
+public class DbUtil {
+	
+	public Connection getCon() throws Exception {
+		Class.forName(PropertiesUtil.getValue("jdbcName"));
+		Connection con = DriverManager.getConnection(PropertiesUtil.getValue("dbUrl"), PropertiesUtil.getValue("dbUserName"), PropertiesUtil.getValue("dbPassword"));
+		return con;
+	}
+	
+	public void closeCon(Connection con) throws Exception {
+		if(con!=null) {
+			con.close();
+		}
+	}
+	
+	public static void main(String[] args) {
+		DbUtil dbUtil = new DbUtil();
+		try {
+			dbUtil.getCon();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			System.out.println("no");
+			e.printStackTrace();
+		}
+	}
+}
---
+++ src/com/util/PropertiesUtil.java
@@ -0,0 +1,21 @@
+package com.util;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+import com.util.PropertiesUtil;
+
+public class PropertiesUtil {
+	
+	public static String getValue(String key) {
+		Properties  prop = new Properties();
+		InputStream in = new PropertiesUtil().getClass().getResourceAsStream("/stuManage.properties");
+		try {
+			prop.load(in);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return (String)prop.get(key);
+	}
+}
